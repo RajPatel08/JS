0d6458d2918a3511cc796db6442d1132810553ff
RajPatel08 <indop7939@gmail.com>
2024-12-17 15:14:59 +0530

    done

---
+++ resume.js
@@ -0,0 +1,21 @@
+document.getElementById("name").textContent = `Raj Patel`;
+document.getElementById(
+  "Unen"
+).textContent = `B.Tech in Information Technology`;
+
+document.getElementById(
+  "Unname"
+).textContent = `Parul University, 2022 - Present`;
+
+document.getElementById("scen").textContent = `Senior Secondary Education`;
+
+document.getElementById(
+  "scname"
+).textContent = `LMP School, 2020 - 2022`;
+
+document.getElementById("p1").textContent = `Ai Paper `;
+
+document.getElementById(
+  "p1d"
+).textContent = ` developed an interactive language learning platform that merges entertainment with education. 
+The platform allows users to learn languages using real-world contexts such as movie and series clips. It includes engaging tests and activities to assess and track user progress, fostering an immersive learning experience.`;
